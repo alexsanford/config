b36df261553571960f2b513eb2bc3556c57908dd
Alex Sanford <alex.sanford1@gmail.com>
2013-01-31 14:04:21 -0400

    Made ruby puts prompt optional

--- ruby/puts_prompt/puts_prompt.rb
+++ ruby/puts_prompt/puts_prompt.rb
@@ -1,9 +1,17 @@
 
 module Kernel
 
+  def puts_prompt
+    ENV['RUBY_PUTS_PROMPT'] = 'yes'
+  end
+
   def puts_with_prefix(*args)
-    prefix = "\e[1;36m>>>\e[0m "
-    puts_without_prefix(args.collect { |str| "#{prefix}#{str}" })
+    if ENV['RUBY_PUTS_PROMPT'] == 'yes'
+      prefix = "\e[1;36m>>>\e[0m "
+      puts_without_prefix(*(args.collect { |str| "#{prefix}#{str}" }))
+    else
+      puts_without_prefix(*args)
+    end
   end
   alias :puts_without_prefix :puts
   alias :puts :puts_with_prefix
