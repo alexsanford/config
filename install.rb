485f7b71f2210afc004092f0e0ea519e16605b5c
Alex Sanford <alex.sanford1@gmail.com>
2017-04-13 10:06:25 -0300

    Add editorconfig

--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 $mydir = File.expand_path(File.dirname(__FILE__))
-$files = %w{zshrc zsh_aliases zshenv vimrc gvimrc vim tmux.conf gitconfig ruby}
+$files = %w{zshrc zsh_aliases zshenv vimrc gvimrc vim tmux.conf gitconfig ruby editorconfig}
 $extensions = %w{osx linux}
 
 def link(file)
